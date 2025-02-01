fd219c32fe32c77d0adced75f4a831bb93ae5bbe
littleyu0820 <67886913+littleyu0820@users.noreply.github.com>
2025-02-01 16:21:05 +0800

    Update Binary_Search.py

--- Exercise/Binary_Search.py
+++ Exercise/Binary_Search.py
@@ -1,13 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        left, right = 0, len(nums) - 1 #[left, right]
         while(left <= right):
             middle = (left + right)//2
-            if(nums[middle] > target):
+            if(nums[middle] > target): #left side
                 right = middle - 1
-            elif(nums[middle] < target):
+            elif(nums[middle] < target): #right side
                 left = middle + 1
             else:
-                return middle
+                return middle # The Index of the target
             
         return -1
